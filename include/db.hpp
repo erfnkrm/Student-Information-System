38bafac4d8d12cbce97ce6443b4b6ad51e916d51
Erfan karimi <erfan.karimi@stu.fbu.edu.tr>
2026-01-07 17:01:57 +0300

    change to the correct and real db

---
+++ include/db.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <pqxx/pqxx>
+
+#include "student.hpp"
+
+class Database {
+ public:
+  explicit Database(std::string conninfo);
+
+  void ensure_schema();
+
+  void insert_student(const Student& s);
+  std::vector<Student> list_students();
+  void update_student(const Student& s);
+  void delete_student(int student_id);
+
+ private:
+  std::unique_ptr<pqxx::connection> conn_;
+  std::string conninfo_;
+
+  void reconnect_if_needed();
+};
+
